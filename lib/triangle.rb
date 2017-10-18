8813572273e32e3cc362ddb2b4fe71a004ad7f95
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-18 01:12:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
+require 'pry'
+
 class Triangle
   # write code here
+
+  def initialize(side_one, side_two, side_three)
+    @sides = [side_one, side_two, side_three]
+  end
+
+  def kind
+    binding.pry
+    boolean = @sides.combination(2).any? {|a, b| a == b }
+      if boolean == true
+        :equilateral
+      end
+  end
+
+end
+
+
+class TriangleError < StandardError
+ # triangle error code
 end
