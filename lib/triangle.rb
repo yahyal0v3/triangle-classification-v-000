de507108bf70c65f7f5e836fc57c4395d22cd6bd
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-10-18 02:09:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   def kind
     kind = nil
     @sides.combination(3).each do |a, b, c|
-      if a = 0 || b = 0 || c = 0
+      if a == 0 || b == 0 || c == 0
         raise TriangleError
       elsif a + b <= c || b + c <= a || a + c <= b
         raise TriangleError
